c1e282a9a9d46f38e60c00c311a445c2cb3a0c4b
thesatyam05 <satyamwakchaure2002@gmail.com>
2023-11-06 14:05:43 +0530

    Fix no border on input

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -18,7 +18,8 @@ const Input = (props: Props) => {
 					type={props.type}
 					autoComplete='true'
 					required
-					className='block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+					style={{ border: '1px solid #e5e7eb' }}
+					className='block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
 				/>
 			</div>
 		</div>
